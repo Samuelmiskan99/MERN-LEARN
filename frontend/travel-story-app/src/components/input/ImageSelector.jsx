8251c6b66b70365febabdb089afa00343d5ad3ca
Bloodymary14400 <miskanhanock@gmail.com>
2024-11-02 14:05:58 +0700

    Add Travel Story Function

--- frontend/travel-story-app/src/components/input/ImageSelector.jsx
+++ frontend/travel-story-app/src/components/input/ImageSelector.jsx
@@ -1,5 +1,78 @@
-const ImageSelector = () => {
-   return <div>ImageSelector</div>
+import { useEffect, useRef, useState } from 'react'
+import { FaRegFileImage } from 'react-icons/fa'
+import { MdDeleteOutline } from 'react-icons/md'
+
+const ImageSelector = ({ image, setImage, handleDeleteImg }) => {
+   const inputRef = useRef(null)
+   const [previewUrl, setPreviewUrl] = useState(null)
+
+   const handleImageChange = (event) => {
+      const file = event.target.files[0]
+      if (file) {
+         setImage(file)
+      }
+   }
+   const onChooseFile = () => {
+      inputRef.current.click()
+   }
+
+   const handleRemoveImage = () => {
+      setImage(null)
+      handleDeleteImg()
+   }
+
+   useEffect(() => {
+      //if the image prop is a string (URL) then set the previewUrl
+      if (typeof image === 'string') {
+         setPreviewUrl(image)
+      } else if (image) {
+         //if the image prop is an object (file) then set the previewUrl
+         setPreviewUrl(URL.createObjectURL(image))
+      } else {
+         //if the image prop is null then set the previewUrl to null
+         setPreviewUrl(null)
+      }
+      return () => {
+         if (previewUrl && typeof previewUrl === 'string') {
+            URL.revokeObjectURL(previewUrl)
+         }
+      }
+   }, [image])
+
+   return (
+      <div>
+         <input
+            type='file'
+            className='hidden'
+            accept='image/*'
+            ref={inputRef}
+            onChange={handleImageChange}
+         />
+         {!image ? (
+            <button
+               className='w-full h-[220px] flex flex-col items-center justify-center gap-4 bg-slate-50 rounded border border-slate-200/50'
+               onClick={() => onChooseFile()}>
+               <div className='w-14 h-14 flex items-center justify-center bg-cyan-50/40 rounded-full border-cyan-100'>
+                  <FaRegFileImage className='text-xl text-cyan-500' />
+               </div>
+               <p className='text-sm text-slate-500'>Browse an image to upload</p>
+            </button>
+         ) : (
+            <div className='w-full relative'>
+               <img
+                  src={previewUrl}
+                  alt='Selected'
+                  className='w-full h-[300px] object-cover rounded-lg'
+               />
+               <button
+                  className='btn-small btn-delete absolute top-2 right-2'
+                  onClick={handleRemoveImage}>
+                  <MdDeleteOutline className='text-lg' />
+               </button>
+            </div>
+         )}
+      </div>
+   )
 }
 
 export default ImageSelector
--- frontend/travel-story-app/src/pages/Home/AddEditTravelStory.jsx
+++ frontend/travel-story-app/src/pages/Home/AddEditTravelStory.jsx
@@ -2,6 +2,11 @@ import { MdAdd, MdClose, MdUpdate, MdDeleteOutline } from 'react-icons/md'
 import DateSelector from '../../components/input/DateSelector'
 import { useState } from 'react'
 import ImageSelector from '../../components/input/ImageSelector'
+import TagInput from '../../components/input/TagInput'
+import moment from 'moment'
+import axiosInstance from '../../utils/axiosInstance'
+import uploadImage from '../../utils/uploadImage'
+import { toast } from 'react-toastify'
 
 const AddEditTravelStory = ({ storyInfo, type, onClose, getAllTravelStories }) => {
    const [title, setTitle] = useState('')
@@ -9,9 +14,62 @@ const AddEditTravelStory = ({ storyInfo, type, onClose, getAllTravelStories }) =
    const [story, setStory] = useState('')
    const [visitedLocation, setVisitedLocation] = useState([])
 
+   const [error, setError] = useState('')
    const [visitedDate, setVisitedDate] = useState(null)
 
-   const handleAddorUpdateClick = () => {}
+   //add new Travel Story
+   const addNewTravelStory = async () => {
+      try {
+         let imageUrl = ''
+
+         //upload image if present
+         if (storyImg) {
+            const imageUpLoadRes = await uploadImage(storyImg)
+            //get image url
+            imageUrl = imageUpLoadRes.imageUrl || ''
+         }
+         const response = await axiosInstance.post('/add-travel-story', {
+            title,
+            story,
+            visitedLocation,
+            imageUrl: imageUrl || '',
+            visitedDate: visitedDate ? moment(visitedDate).valueOf() : moment().valueOf(),
+         })
+         if (response.data && response.data.story) {
+            toast.success('Story Added  Successfully')
+            //refresh Stories
+            getAllTravelStories()
+            //close modal or form
+            onClose()
+         }
+      } catch (error) {
+         setError(error.message)
+      }
+   }
+   //update Travel Story
+   const updateTravelStory = async () => {}
+
+   const handleAddorUpdateClick = () => {
+      console.log('input Data', { title, storyImg, story, visitedLocation, visitedDate })
+      if (!title) {
+         setError('Please enter the title')
+         return
+      }
+      if (!story) {
+         setError('Please enter the story')
+         return
+      }
+      setError('')
+      if (type === 'edit') {
+         updateTravelStory()
+      } else {
+         addNewTravelStory()
+      }
+   }
+
+   //handle delete story and update story
+   const handleDeleteStoryImg = async () => {}
+
    return (
       <div>
          <div className='flex items-center justify-between '>
@@ -35,6 +93,7 @@ const AddEditTravelStory = ({ storyInfo, type, onClose, getAllTravelStories }) =
                      <MdClose className='text-xl text-slate-400' />
                   </button>
                </div>
+               {error && <p className='text-xs pt-2 text-right text-red-500'>{error}</p>}
             </div>
          </div>
          <div>
@@ -52,7 +111,11 @@ const AddEditTravelStory = ({ storyInfo, type, onClose, getAllTravelStories }) =
                </div>
 
                {/* Image selector */}
-               <ImageSelector image={storyImg} setImage={setStoryImg} />
+               <ImageSelector
+                  image={storyImg}
+                  setImage={setStoryImg}
+                  handleDeleteImg={handleDeleteStoryImg}
+               />
 
                <div className='flex flex-col gap-2 mt-4'>
                   <label className='input-label'>Story</label>
@@ -62,7 +125,12 @@ const AddEditTravelStory = ({ storyInfo, type, onClose, getAllTravelStories }) =
                      placeholder='Your Story'
                      rows={10}
                      value={story}
-                     onChange={(e) => setStory(e.target.value)}></textarea>
+                     onChange={(e) => setStory(e.target.value)}
+                  />
+               </div>
+               <div className='pt-3'>
+                  <label className='input-label'>Visited Location</label>
+                  <TagInput tags={visitedLocation} setTags={setVisitedLocation} />
                </div>
             </div>
          </div>
